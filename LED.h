af990bfa30e316e38c14d894d40ffa16d2c7ed65
Michael Wood <mnet@awoodfamily.com>
2017-01-06 21:10:59 -0800

    Create LED.h

---
+++ LED.h
@@ -0,0 +1,23 @@
+#ifndef LED_H__INCLUDED
+#define LED_H__INCLUDED
+
+
+#include "WPILib.h"
+
+
+class LED{
+public:
+	LED(void);
+	~LED(void);
+	
+	enum Color {Red, Green, Blue, Purple, Aqua, Yellow, White, Off};
+	void ChangeColor(Color);
+	
+private:
+	Relay* oRed;
+	Relay* oGreen;
+	Relay* oBlue;
+};
+
+
+#endif /* LED_H__INCLUDED */
